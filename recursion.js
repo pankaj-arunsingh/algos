7391116943b8b72afcb0e02e21d062d1ed891f1a
pankaj-arunsingh <pankaj_arunsingh@ymail.com>
2021-07-20 09:55:11 +1000

    added more examples

--- recursion.js
+++ recursion.js
@@ -69,3 +69,70 @@ const recursiveFactorial = (num) => {
   return result;
 };
 console.log(recursiveFactorial(5));
+let company = {
+  // the same object, compressed for brevity
+  sales: [
+    { name: "John", salary: 1000 },
+    { name: "Alice", salary: 1600 },
+  ],
+  development: {
+    sites: [
+      { name: "Peter", salary: 2000 },
+      { name: "Alex", salary: 1800 },
+    ],
+    internals: [{ name: "Jack", salary: 1300 }],
+  },
+};
+function sumSalaries(department) {
+  if (Array.isArray(department)) {
+    // case (1)
+    return department.reduce((prev, current) => prev + current.salary, 0); // sum the array
+  } else {
+    // case (2)
+    let sum = 0;
+
+    for (let subdep of Object.values(department)) {
+      console.log("subdep", subdep);
+      sum += sumSalaries(subdep); // recursively call for subdepartments, sum the results
+    }
+    return sum;
+  }
+}
+console.log("sum of salaries", sumSalaries(company));
+
+let list = {
+  value: 1,
+  next: {
+    value: 2,
+    next: {
+      value: 3,
+      next: {
+        value: 4,
+        next: null,
+      },
+    },
+  },
+};
+
+function printReverseList(list) {
+  if (list.next) {
+    printReverseList(list.next);
+  }
+
+  console.log(list.value);
+}
+function printList(list) {
+  console.log(list.value);
+  if (list.next) {
+    printList(list.next);
+  }
+}
+printReverseList(list);
+printList(list);
+
+function fib(n) {
+  return n <= 1 ? n : fib(n - 1) + fib(n - 2);
+}
+
+console.log(fib(3)); // 2
+console.log(fib(7)); // 13
